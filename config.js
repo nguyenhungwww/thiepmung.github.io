8bef5344080e0d6933fd9c3e24aceefb40ba6774
nguyenhungwww <158760425+nguyenhungwww@users.noreply.github.com>
2024-02-03 23:20:32 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = 'Mấy Nủy';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Năm Mới';
+const titleCard = 'Tặng Mấy nủy';
+const contentCard = '새해 복 많이 받으세요';
+
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hue1.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
